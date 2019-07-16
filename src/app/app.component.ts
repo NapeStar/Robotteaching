fce7104d2a9fd1d3498a04b95eb5f4fe65b6b15b
jopihabich <jophab96>
2019-07-16 11:20:41 +0200

    HTTP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'RobotTeaching';
+  title = 'RobotTeachingv0';
+
+  private avvMethods  = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  doPostRequest() {
+    const req = this.http.post('http://localhost:4000', {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    })
+      .subscribe(
+        (res: any[]) => {
+          console.log(res);
+          this.avvMethods = res;
+        },
+        err => {
+          console.log("Error occured");
+        }
+      );
+  }
+
 }
