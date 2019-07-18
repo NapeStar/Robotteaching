b9ed7bf0102fbe3e33e8f57489ee61fd9f802f1b
mnapetsc <mnapetsc@edu.aau.at>
2019-07-18 12:43:31 +0200

    try drag and drop Issue -> return Value (Object) from HTTP Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {DataService} from './data.service';
+import {Workflow} from './data.workflow';
 
 
 @Component({
@@ -9,44 +10,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 
 
-export class AppComponent {
-  title = 'RobotTeachingv0';
-
-   avvMethods  = [];
-
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-
-      'jsonrpc':'2.0',
-      'user':'intern',
-      'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2JvdElkIjoiY2hpbWVyYTEiLCJle' +
-        'HBpcmVzIjozMTUzNjAwMH0.fPubN5HhuKhmg0o8gL5NA7TCNbtLdL6FxkG_B8A3U1s'
-
-    })
-  };
-
-
-  constructor(private http: HttpClient) {
-  }
-
-
-  doPostRequest() {
-    const req = this.http.post('http://localhost:4000', {
-      "jsonrpc":"2.0",
-      "method":"get_service_description",
-      "params":[3],
-      "id": 1
-    },this.httpOptions)
-      .subscribe(
-        (res: any[]) => {
-          console.log(res);
-          this.avvMethods = res;
-        },
-        err => {
-          console.log("Error occured");
-        }
-      );
-  }
+export class AppComponent{
+ titel = 'Welcome To Robot Teaching';
 
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+
+      'jsonrpc':'2.0',
+      'user':'intern',
+      'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2JvdElkIjoiY2hpbWVyYTEiLCJle' +
+        'HBpcmVzIjozMTUzNjAwMH0.fPubN5HhuKhmg0o8gL5NA7TCNbtLdL6FxkG_B8A3U1s'
+    })
+  };
+
+
+  constructor(private http: HttpClient) { }
+
+  getWorkflows(){
+    return this.http.post('http://localhost:4000', {
+      'jsonrpc': '2.0',
+      'method': 'get_available_workflows',
+      'params': [3],
+      'id': '1'
+    },this.httpOptions);
+  }
+}
