4d44439af87f1258d799dc60451579060cfeb863
mnapetsc <mnapetsc@edu.aau.at>
2020-03-27 14:28:32 +0100

    update doku for websocket and run component-> progress spinner

--- src/app/execution/execution-run/execution-run.component.ts
+++ src/app/execution/execution-run/execution-run.component.ts
@@ -13,7 +13,11 @@ import {WizardParentStepperService} from '../../wizard-stepper/wizard-parent/wiz
 import {SocketDataService} from '../../sockets/websocket.service';
 import {NgxSpinnerService} from 'ngx-spinner';
 
-
+/**
+ * This component shows the configurated workflow and list all containing jobs.
+ *
+ * It communicates with backend for saving, updating, and executing the workflow
+ */
 @Component({
   selector: 'app-execution-run',
   templateUrl: './execution-run.component.html',
@@ -21,17 +25,40 @@ import {NgxSpinnerService} from 'ngx-spinner';
 })
 export class ExecutionRunComponent extends WizardJobComponent implements OnInit, OnDestroy {
 
+  /**
+   * before workflow ist not saved run button not displayed
+   */
   isDisabledRun = true;
+  /**
+   * status if already saved in backend
+   */
   isAlreadySave = false;
-  isLoading = false;
+  // isLoading = false;
   workflowProgress = 0;
+  /**
+   * displayed text of spinner
+   */
   spinnerText: string;
+  /**
+   * status for spinner
+   */
   displaySpinner = false;
-
+  /**
+   * massage for websocket
+   */
   message: string;
-  messages: string[] = [];
-  connection;
+  // messages: string[] = [];
+  // connection;
 
+  /**
+   * constructor - calls constructor of parent WizardJobComponent
+   * @param {Router} router For redirecting
+   * @param {WizardStepperService} wizardStepperService For Sharing Workflow Information
+   * @param {HttpRequestService} httpRequest For getting actual position from robot arm
+   * @param {WizardParentStepperService} eventEmitterService For Sharing Angular Material Stepper View
+   * @param {SocketDataService} SocketDataService For getting actual progress from backend
+   * @param {NgxSpinnerService} spinner For Progress Spinner
+   */
   constructor(protected router: Router,
               protected wizardStepperService: WizardStepperService,
               private http: HttpClient,
@@ -43,26 +70,35 @@ export class ExecutionRunComponent extends WizardJobComponent implements OnInit,
   ) {
     super(router, wizardStepperService, eventEmitterService);
   }
-
+  /**
+   * ngOnInit is a lifecycle hook - executed after constructor
+   *
+   * overrides parent ngOninit() declares additional necessary variables for this component (WebSocket for Spinner)
+   */
   ngOnInit() {
     super.ngOnInit();
-
     this.SocketDataService.onNewMessage().subscribe(msg => {
       console.log('Workflow Progress: ' + msg);
       this.setWorkflowProgress(msg);
     });
   }
-
+  /**
+   * activates spinner in view
+   */
   showSpinner() {
     this.spinner.show();
   }
-
+  /**
+   * hides spinner in view
+   */
   hideSpinner(){
     this.spinner.hide();
   }
-
+  /**
+   * Sets worflowProgess for spinner
+   * @param {any} msg
+   */
   setWorkflowProgress(msg) {
-
     this.workflowProgress = parseInt(msg, 10);
     this.spinnerText = 'Executing Workflow! Finished: ' + this.workflowProgress +' %';
     if (this.workflowProgress === 100) {
@@ -70,7 +106,12 @@ export class ExecutionRunComponent extends WizardJobComponent implements OnInit,
       this.workflowProgress = 0;
     }
   }
-
+  /**
+   * When clicked on Play button.
+   * Activates spinner,
+   * communicates with backend -> send execution request and
+   * ask for progress via socket
+   */
   runOnClick() {
     this.showSpinner();
     this.spinnerText = 'Executing Workflow! Finished: 0%';
@@ -83,6 +124,11 @@ export class ExecutionRunComponent extends WizardJobComponent implements OnInit,
       console.log('Hello from setTimeout');
     }, 5000);
   }
+  /**
+   * When 1st creation und clicked on Save button.
+   * Communicates with backend -> send workflow to be saved and
+   * receives workflow ID from backend
+   */
   saveCreateOnClick() {
     this.eventEmitterService.onStepperNextClick();
     this.isDisabledRun = false;
@@ -93,7 +139,10 @@ export class ExecutionRunComponent extends WizardJobComponent implements OnInit,
     }
     console.log('Next Create wurde ausgeführt');
   }
-
+  /**
+   * When already stored Workflow and clicked on Save button.
+   * Communicates with backend -> send workflow to be updated and
+   */
   saveUpdateOnClick() {
     this.eventEmitterService.onStepperNextClick();
     this.isDisabledRun = false;
@@ -104,21 +153,33 @@ export class ExecutionRunComponent extends WizardJobComponent implements OnInit,
     }
     console.log('Next Update wurde ausgeführt');
   }
-
+  /**
+   * When 1st creation and clicked on Back button.
+   *
+   * redirects accordingly and reset counter
+   */
   backCreateOnClick() {
     this.eventEmitterService.onStepperBackClick();
     this.selectNextJob(this.workflow.getJobName(this.counter));
     this.router.navigate([this.link]);
     console.log('Back Create wurde ausgeführt');
   }
-
+  /**
+   * When updated Workflow and clicked on Back button.
+   *
+   * redirects accordingly and reset counter
+   */
   backUpdateOnClick() {
     this.eventEmitterService.onStepperBackClick();
     this.selectNextJob(this.workflow.getJobName(this.counter));
     this.router.navigate([this.link]);
     console.log('Back Update wurde ausgeführt');
   }
-
+  /**
+   * When executing Workflow and clicked on Back button.
+   *
+   * redirects accordingly and reset counter
+   */
   backRunOnClick() {
     this.link = '';
     this.router.navigate([this.link]);
--- src/app/sockets/websocket.service.ts
+++ src/app/sockets/websocket.service.ts
@@ -4,29 +4,31 @@ import {Observable, Observer} from 'rxjs';
  * This service provides a websocket needed for viewing progress of backend
  */
 export class SocketDataService {
+  /**
+   * url
+   */
   private url = 'http://localhost:3030';
+  /**
+   * web socket
+   */
   private socket;
-
+  /**
+   * default constructor - instantiate socket
+   */
   constructor() {
     this.socket = io(this.url);
   }
-
+  /**
+   * not used, but you can send a message to the socket
+   * @param {any} message The message
+   */
   public sendMessage(message) {
     this.socket.emit('new-message', message);
   }
-
-  /*
-    public getMessages = () => {
-      return Observable.create((observer) => {
-        this.socket.on('new-message', (message) => {
-          console.log("NEW MSG FROM SERVER");
-          console.log(message);
-          observer.next(message);
-        });
-      });
-    }
-    */
-
+  /**
+   * if status change of obeservable
+   * @returns The Execution Status Msg
+   */
   onNewMessage() {
     return new Observable((observer) => {
       this.socket.on('message', msg => {
